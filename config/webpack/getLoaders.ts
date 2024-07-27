2d0fbf419a4f1193674263cb6ff444c202163e91
BITW <gordeybitw@gmail.com>
2024-07-27 17:34:32 +0300

    css module

--- config/webpack/getLoaders.ts
+++ config/webpack/getLoaders.ts
@@ -8,10 +8,25 @@ export const getLoaders = (): ModuleOptions["rules"] => {
     exclude: /node_modules/,
   }; // ts loader из коробки умеет обращаться с jsx, если бы его не было то нужно было бы настроить babel-loader
 
-  const cssLoader = {
+  // TODO разобраться с css module
+
+  // const cssLoader = {
+  //   test: /\.css$/i,
+  //   loader: "css-loader",
+  //   options: {
+  //     modules: {
+  //       mode: "local",
+  //       auto: true,
+  //       exportGlobals: true,
+  //       localIdentName: "[path][name]__[local]--[hash:base64:5]",
+  //     },
+  //   },
+  // };
+
+  const allCssLoaders = {
     test: /\.css$/i,
     use: [MiniCssExtractPlugin.loader, "css-loader"],
   };
 
-  return [tsLoader, cssLoader];
+  return [tsLoader, allCssLoaders];
 };
--- src/components/features/App/index.tsx
+++ src/components/features/App/index.tsx
@@ -1,6 +1,39 @@
-import React from "react";
-import "./styled.css";
+import React, { useState } from "react";
+import * as classes from "./styled.module.css";
+
+interface UserInfo {
+  name: string;
+  about: string;
+}
 
 export const App = () => {
-  return <div className="backpack">App</div>;
+  const [userInfo, userInfoChanged] = useState<UserInfo>({
+    name: "",
+    about: "",
+  });
+
+  const userInputsHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    userInfoChanged((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.inputsContainer}>
+        <input
+          className={classes.userInput}
+          name="name"
+          value={userInfo.name}
+          onChange={userInputsHandler}
+          placeholder="Введите имя"
+        />
+        <input
+          className={classes.userInput}
+          name="about"
+          value={userInfo.about}
+          onChange={userInputsHandler}
+          placeholder="Введите краткую информацию"
+        />
+      </div>
+    </div>
+  );
 };
